fe2493447fc7578710d513b6f5c968035bc0eecf
renat <mitsoftwarecompany@gmail.com>
2020-05-19 10:29:49 +0300

    Добавил сслыки

--- src/components/presentational/Footer.js
+++ src/components/presentational/Footer.js
@@ -9,12 +9,12 @@ function Main() {
     <footer className="Footer">
       <div className="FooterWr">
         <Link href="/" className="FooterA">Соглашение о пользовании системой © 2020 ООО «SomeName». Все права защищены.</Link><br />
-        <Link href="mailto:support@somename.ru" target="_blank" className="FooterA" >support@somename.ru</Link><br />
+        <a href="mailto:support@somename.ru" target="_blank" className="FooterA" >support@somename.ru</a><br />
         <div className='FooterLine'></div>
         <div className="socialMedia">
-        <TwitterCircleFilled />
-        <FacebookFilled />
-        <InstagramFilled />
+          <a href="https://twitter.com" target="_blank"><TwitterCircleFilled /></a>
+          <a href="https://www.facebook.com" target="_blank"><FacebookFilled /></a>
+          <a href="https://www.instagram.com" target="_blank"><InstagramFilled /></a>
       </div>
       </div>
     </footer>
